4536c86b88c23e10c7ad5fa9def1b0b27a40606c
Koidy <yuji.koido@gmail.com>
2019-12-23 03:15:08 +0900

    Update production.rb
    本番環境のみでBasic認証をするようにする。

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,8 @@
 server '18.177.66.238', user: 'ec2-user', roles: %w{app db web}
 
-# set :rails_env, "production"
-# set :unicorn_rack_env, "production"
+# 本番環境のみにBasic認証をするようにする。
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # server-based syntax
 # ======================
