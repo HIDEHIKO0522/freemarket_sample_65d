47dda89e185e5a0b0ca3fbe74d4b68dc95ace77e
akinori0714 <akinori_ichimura@yahoo.co.jp>
2019-12-24 12:45:46 +0900

    ルート設定しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'home#index'
   # get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users do
+    collection do
+      get 'logout'
+    end
+  end
+  get 'users/identification(/:id)', to: 'users#identification', as: :user_identification
+  resources :items
+  get 'items/confirm(/:id)', to: 'items#confirm', as: :items_confirm
+  resources :cards
 
-end
+
+end 
