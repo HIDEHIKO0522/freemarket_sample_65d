ff2ec4176cbc4eca66123ceb08e604e0467b9e18
a-kitajima <57699446+a-kitajima@users.noreply.github.com>
2019-12-24 17:06:52 +0900

    商品登録機能実装（画像以外）
    商品画像以外の要素を登録する機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    binding.pry
+    if @item.save
+      redirect_to new_item_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :comment, :condition, :brand, :size, :price, :arrival_date, :charge, :location, :delivery).merge(seller_id: current_user.id, category_id: 1)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :buyer, class_name: 'User'
+  belongs_to :buyer, class_name: 'User', optional: true
   belongs_to :seller, class_name: 'User'
   has_many :item_images
   belongs_to :category
