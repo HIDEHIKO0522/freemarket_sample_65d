bb1ce13e5645b7b9530b5fe001ac7feca0ffb10d
Koidy <yuji.koido@gmail.com>
2020-01-03 16:32:45 +0900

    追加修正
    サーバーサイドの設定に合わせて記述したが一部コメントアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,32 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #'registration' ,'sms_authentication' ,'address' ,'card' ,'done' のみルーティングをする
+  devise_for :users,skip: :all
+  
+  devise_scope :user do
+    delete 'destroy' => 'devise/sessions#destroy',as: :current_user_destroy
+  end
+
   root to: 'home#index'
   # get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :signup ,only: [:index,:create] do
+    collection do
+      get 'registration'                               
+      post 'registration' => 'signup#profile_validation' 
+      get 'sms_authentication' 
+      post 'sms_authentication' => 'signup#sms_validation' 
+      # get 'sms_confirmation' 
+      # post 'sms_confirmation' => 'signup#sms_check'
+      get 'address' 
+      post 'address' => 'signup#address_validation' 
+      get 'card' 
+      post 'card' => 'signup#card_validation' 
+      get 'done' 
+    end
+  end  
+
+
 
   resources :users do
     collection do
