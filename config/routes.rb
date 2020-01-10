969706cb0c9580faf71917a24cdb7eb921575ca7
Koidy <yuji.koido@gmail.com>
2020-01-10 15:42:11 +0900

    追加修正（omniauth）
    deviseのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   
-  devise_for :users,skip: :all
+  devise_for :users,
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: "users/registrations",
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   
   devise_scope :user do
     get 'login', to: 'devise/sessions#new', as: :new_user_session
