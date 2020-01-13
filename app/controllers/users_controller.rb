89c4edba29040a5b15d789c321cccea7939575a7
Koidy <yuji.koido@gmail.com>
2020-01-14 01:08:32 +0900

    追加修正（ユーザーログイン機能）
    現在のマスターで実装されている機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,15 @@ class UsersController < ApplicationController
     @sell_items = Item.where(seller_id: @user.id).order(updated_at: :desc)
   end
 
+  def logout
+  end  
+
+  def card_registration
+  end
+  
+  def card_information
+  end  
+
+  def certification
+  end
 end
