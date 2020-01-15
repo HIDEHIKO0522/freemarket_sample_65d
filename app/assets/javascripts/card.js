1aac12b492ba913826bbe4d64013ade2969ac2d5
a-kitajima <57699446+a-kitajima@users.noreply.github.com>
2020-01-15 17:11:40 +0900

    不要コード削除

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,7 +1,6 @@
 $(document).on('turbolinks:load', function() {
   var form = $("#new_card");
   $('#new_card').on("click", "#card-submit-button", function() {
-    // e.preventDefault();
     Payjp.setPublicKey('pk_test_fb1ae90bf0b9bcca1ba9f8db');
     form.find("input[type=submit]").prop("disabled", true);
     var number = form.find('input[name="card[number]"]'),
