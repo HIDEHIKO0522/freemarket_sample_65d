a471221871135d0e087866d7a9d0e0e542f1073c
a-kitajima <57699446+a-kitajima@users.noreply.github.com>
2020-01-16 18:35:59 +0900

    カードモデルテスト日本語表記に修正

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,31 +3,31 @@ require 'rails_helper'
 describe Card do
   describe '#create' do
 
-    it "numberが空では登録できないこと" do
+    it "カード番号が空では登録できないこと" do
       card = build(:card, number: "")
       card.valid?
       expect(card.errors[:number]).to include("can't be blank")
     end
 
-    it "security_codeが空では登録できないこと" do
+    it "セキュリティコードが空では登録できないこと" do
       card = build(:card, security_code: "")
       card.valid?
       expect(card.errors[:security_code]).to include("can't be blank")
     end
 
-    it "expiration_monthが空では登録できないこと" do
+    it "有効期限（月）が空では登録できないこと" do
       card = build(:card, expiration_month: "")
       card.valid?
       expect(card.errors[:expiration_month]).to include("can't be blank")
     end
 
-    it "expiration_yearが空では登録できないこと " do
+    it "有効期限（年）が空では登録できないこと " do
       card = build(:card, expiration_year: "")
       card.valid?
       expect(card.errors[:expiration_year]).to include("can't be blank")
     end
 
-    it "tokenが空では登録できないこと " do
+    it "トークンが空では登録できないこと " do
       card = build(:card, token: "")
       card.valid?
       expect(card.errors[:token]).to include("can't be blank")
